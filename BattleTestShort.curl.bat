8cbdf6fcc1b349cfe2bef3e54dac67a8fd3d2f30
if23b288 <if23b288@technikum-wien.at>
2024-12-14 13:50:47 +0100

    added more users and battles

--- BattleTestShort.curl.bat
+++ BattleTestShort.curl.bat
@@ -20,6 +20,8 @@ echo "Should return HTTP 201"
 echo.
 curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"admin\",    \"Password\":\"istrator\"}"
 echo "Should return HTTP 201"
+curl -i -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"test\",    \"Password\":\"user\"}"
+echo "Should return HTTP 201"
 echo.
 
 REM --------------------------------------------------
@@ -32,27 +34,31 @@ echo "should return HTTP 200 with generated token for the user, here: altenhof-m
 echo.
 curl -i -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"admin\",    \"Password\":\"istrator\"}"
 echo "should return HTTP 200 with generated token for the user, here: admin-mtcgToken"
+curl -i -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"test\",    \"Password\":\"user\"}"
+echo "should return HTTP 200 with generated token for the user, here: test-mtcgToken"
 echo.
 
 REM --------------------------------------------------
 echo 3) create packages (done by "admin")
 curl -i -X POST http://localhost:10001/packages --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d "[{\"Id\":\"845f0dc7-37d0-426e-994e-43fc3ac83c08\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"99f8f8dc-e25e-4a95-aa2c-782823f36e2a\", \"Name\":\"Dragon\", \"Damage\": 50.0}, {\"Id\":\"e85e3976-7c86-4d06-9a80-641c2019a79f\", \"Name\":\"WaterSpell\", \"Damage\": 20.0}, {\"Id\":\"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Name\":\"Ork\", \"Damage\": 45.0}, {\"Id\":\"171f6076-4eb5-4a7d-b3f2-2d650cc3d237\", \"Name\":\"FireSpell\",    \"Damage\": 25.0}]"
 echo "Should return HTTP 201"
-echo.																																																																																		 				    
 curl -i -X POST http://localhost:10001/packages --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d "[{\"Id\":\"aa9999a0-734c-49c6-8f4a-651864b14e62\", \"Name\":\"WaterGoblin\", \"Damage\":  9.0}, {\"Id\":\"d6e9c720-9b5a-40c7-a6b2-bc34752e3463\", \"Name\":\"Dragon\", \"Damage\": 55.0}, {\"Id\":\"d60e23cf-2238-4d49-844f-c7589ee5342e\", \"Name\":\"WaterSpell\", \"Damage\": 21.0}, {\"Id\":\"02a9c76e-b17d-427f-9240-2dd49b0d3bfd\", \"Name\":\"Ork\", \"Damage\": 55.0}, {\"Id\":\"f8043c23-1534-4487-b66b-238e0c3c39b5\", \"Name\":\"WaterSpell\",   \"Damage\": 23.0}]"
 echo "Should return HTTP 201"
-echo.
-
-REM --------------------------------------------------
-echo 4) acquire packages kienboec
-curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
+curl -i -X POST http://localhost:10001/packages --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d "[{\"Id\":\"70962948-2bf7-44a9-9ded-8c68eeac7793\", \"Name\":\"WaterGoblin\", \"Damage\":  9.0}, {\"Id\":\"74635fae-8ad3-4295-9139-320ab89c2844\", \"Name\":\"FireSpell\", \"Damage\": 55.0}, {\"Id\":\"ce6bcaee-47e1-4011-a49e-5a4d7d4245f3\", \"Name\":\"Knight\", \"Damage\": 21.0}, {\"Id\":\"a6fde738-c65a-4b10-b400-6fef0fdb28ba\", \"Name\":\"FireSpell\", \"Damage\": 55.0}, {\"Id\":\"a1618f1e-4f4c-4e09-9647-87e16f1edd2d\", \"Name\":\"FireElf\", \"Damage\": 23.0}]"
+echo "Should return HTTP 201"
+curl -i -X POST http://localhost:10001/packages --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d "[{\"Id\":\"2272ba48-6662-404d-a9a1-41a9bed316d9\", \"Name\":\"WaterGoblin\", \"Damage\": 11.0}, {\"Id\":\"3871d45b-b630-4a0d-8bc6-a5fc56b6a043\", \"Name\":\"Dragon\", \"Damage\": 70.0}, {\"Id\":\"166c1fd5-4dcb-41a8-91cb-f45dcd57cef3\", \"Name\":\"Knight\", \"Damage\": 22.0}, {\"Id\":\"237dbaef-49e3-4c23-b64b-abf5c087b276\", \"Name\":\"WaterSpell\", \"Damage\": 40.0}, {\"Id\":\"27051a20-8580-43ff-a473-e986b52f297a\", \"Name\":\"FireElf\", \"Damage\": 28.0}]"
 echo "Should return HTTP 201"
 echo.
-echo.
 
 REM --------------------------------------------------
-echo 5) acquire packages altenhof
+echo 4) acquire packages
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d ""
+echo "Should return HTTP 201"
 curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer altenhof-mtcgToken" -d ""
+echo "Should return HTTP 201"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d ""
+echo "Should return HTTP 201"
+curl -i -X POST http://localhost:10001/transactions/packages --header "Content-Type: application/json" --header "Authorization: Bearer test-mtcgToken" -d ""
 echo "Should return HTTP 201"
 echo.
 
@@ -60,19 +66,29 @@ REM --------------------------------------------------
 echo 11) configure deck
 curl -i -X PUT http://localhost:10001/deck --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d "[\"845f0dc7-37d0-426e-994e-43fc3ac83c08\", \"99f8f8dc-e25e-4a95-aa2c-782823f36e2a\", \"e85e3976-7c86-4d06-9a80-641c2019a79f\", \"171f6076-4eb5-4a7d-b3f2-2d650cc3d237\"]"
 echo "Should return HTTP 2xx"
-echo.
 curl -i -X PUT http://localhost:10001/deck --header "Content-Type: application/json" --header "Authorization: Bearer altenhof-mtcgToken" -d "[\"aa9999a0-734c-49c6-8f4a-651864b14e62\", \"d6e9c720-9b5a-40c7-a6b2-bc34752e3463\", \"d60e23cf-2238-4d49-844f-c7589ee5342e\", \"02a9c76e-b17d-427f-9240-2dd49b0d3bfd\"]"
+echo "Should return HTTP 2xx"
+curl -i -X PUT http://localhost:10001/deck --header "Content-Type: application/json" --header "Authorization: Bearer admin-mtcgToken" -d "[\"70962948-2bf7-44a9-9ded-8c68eeac7793\", \"74635fae-8ad3-4295-9139-320ab89c2844\", \"ce6bcaee-47e1-4011-a49e-5a4d7d4245f3\", \"a1618f1e-4f4c-4e09-9647-87e16f1edd2d\"]"
+echo "Should return HTTP 2xx"
+curl -i -X PUT http://localhost:10001/deck --header "Content-Type: application/json" --header "Authorization: Bearer test-mtcgToken" -d "[\"3871d45b-b630-4a0d-8bc6-a5fc56b6a043\", \"166c1fd5-4dcb-41a8-91cb-f45dcd57cef3\", \"237dbaef-49e3-4c23-b64b-abf5c087b276\", \"27051a20-8580-43ff-a473-e986b52f297a\"]"
 echo "Should return HTTP 2xx"
 echo.
 
 REM --------------------------------------------------
-echo 17) battle
+echo battles
 start /b "kienboec battle" curl -i -X POST http://localhost:10001/battles --header "Authorization: Bearer kienboec-mtcgToken"
 start /b "altenhof battle" curl -i -X POST http://localhost:10001/battles --header "Authorization: Bearer altenhof-mtcgToken"
+start /b "admin battle" curl -i -X POST http://localhost:10001/battles --header "Authorization: Bearer admin-mtcgToken"
+start /b "test battle" curl -i -X POST http://localhost:10001/battles --header "Authorization: Bearer test-mtcgToken"
+start /b "altenhof battle" curl -i -X POST http://localhost:10001/battles --header "Authorization: Bearer altenhof-mtcgToken"
+start /b "admin battle" curl -i -X POST http://localhost:10001/battles --header "Authorization: Bearer admin-mtcgToken"
 ping localhost -n 10 >NUL 2>NUL
 
 REM --------------------------------------------------
-echo end...
+echo 16) scoreboard
+curl -i -X GET http://localhost:10001/scoreboard --header "Authorization: Bearer kienboec-mtcgToken"
+echo "Should return HTTP 200 - and the scoreboard"
+echo.
 
 REM this is approx a sleep 
 ping localhost -n 100 >NUL 2>NUL
